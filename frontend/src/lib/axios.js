e5de5fc4773a73d76c1d79d2016e8b9cc5851823
RilanRMZ <rilan.rmz@gmail.com>
2025-05-20 15:43:33 +0400

    added build file

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "https://chat-app-server-five-alpha.vercel.app/api",
-  
+  baseURL:
+    import.meta.env.MODE === "development"
+      ? "http://localhost:5001/api"
+      : "https://chatapp-server-2cfu.onrender.com/api",
 });
